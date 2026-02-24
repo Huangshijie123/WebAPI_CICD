5fdeda4caa0e325b07b6ab9c36dc9f0441b1ffee
hsj <huang.shijie1@zhubo.com>
2026-02-24 17:56:27 +0800

    增加swagger功能

--- WebAPI_CICD/Program.cs
+++ WebAPI_CICD/Program.cs
@@ -13,14 +13,26 @@ namespace WebAPI_CICD
             // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
             builder.Services.AddOpenApi();
 
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
             if (app.Environment.IsDevelopment())
             {
                 app.MapOpenApi();
+                app.UseSwagger();
+                app.UseSwaggerUI(c =>
+                {
+                    c.SwaggerEndpoint("/swagger/v1/swagger.json", "WebAPI_CICD v1");
+                    // 如果想让 UI 显示在根路径，取消下一行的注释：
+                    // c.RoutePrefix = string.Empty;
+                });
             }
 
+            
             app.UseHttpsRedirection();
 
             app.UseAuthorization();
